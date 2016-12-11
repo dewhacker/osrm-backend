6c771a707f39f8a98d00f3b39dc5b1a3edae31a4
Kajari <kajari@mapbox.com>
2016-12-12 05:00:25 +0530

    debugging

--- src/engine/plugins/trip.cpp
+++ src/engine/plugins/trip.cpp
@@ -186,6 +186,10 @@ Status TripPlugin::HandleRequest(const std::shared_ptr<const datafacade::BaseDat
         return Status::Error;
     }
 
+    std::for_each(std::begin(result_table), std::end(result_table), [](const auto value) {
+        std::cout << "table_value: " << value << std::endl;
+      });
+
     const constexpr std::size_t BF_MAX_FEASABLE = 10;
     BOOST_ASSERT_MSG(result_table.size() == number_of_locations * number_of_locations,
                      "Distance Table has wrong size");
